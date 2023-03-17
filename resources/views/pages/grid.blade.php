8a5bc83ef018d4a51525e9b853ba0b0600ba05f2
carly <carly.cheung@solutionforest.net>
2023-03-17 11:38:41 +0800

    remove full width (setup on 'config/filament.php' or override protected variable 'maxContentWidth'

--- resources/views/pages/grid.blade.php
+++ resources/views/pages/grid.blade.php
@@ -1,13 +1,3 @@
 <x-filament::page :class="\Illuminate\Support\Arr::toCssClasses(['grid-layout-plugin-view-record-page'])">
-
-    @if ($this->isFullWidth())
-        <style>
-            .filament-main-content {
-                max-width: 100%
-            }
-        </style>
-    @endif
-
-
     {{ $this->grid }}
 </x-filament::page>
--- src/Pages/Grid.php
+++ src/Pages/Grid.php
@@ -38,9 +38,4 @@ abstract class Grid extends Page
 
         return static::grid(Components\Grid::make());
     }
-
-    protected function isFullWidth(): bool
-    {
-        return false;
-    }
 }
