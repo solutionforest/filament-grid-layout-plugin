b7c3dc5b6c912964e775dd06b3469c469f5e21e6
carly <carly.cheung@solutionforest.net>
2023-03-17 11:36:53 +0800

    adjust gap of inner row layout

--- resources/views/components/grid/column.blade.php
+++ resources/views/components/grid/column.blade.php
@@ -5,8 +5,17 @@
     $columns = $column?->getGridColumns() ?? [];
     $components = $column?->getComponents() ?? [];
 @endphp
-<x-filament-support::grid.column :default="$columns['default'] ?? 12" :sm="$columns['sm'] ?? null" :md="$columns['md'] ?? null" :lg="$columns['lg'] ?? null"
-    :xl="$columns['xl'] ?? null" :two-xl="$columns['2xl'] ?? null">
+<x-filament-support::grid.column 
+    :default="$columns['default'] ?? 12" 
+    :sm="$columns['sm'] ?? null" 
+    :md="$columns['md'] ?? null" 
+    :lg="$columns['lg'] ?? null"
+    :xl="$columns['xl'] ?? null" 
+    :two-xl="$columns['2xl'] ?? null"
+    @class([
+        'grid gap-4 lg-gap-8' => in_array(\SolutionForest\GridLayoutPlugin\Components\Grid\Row::class, array_map(fn ($component) => get_class($component), $components))
+    ])
+    >
     @foreach ($components as $component)
         @if (!$component)
             <div></div>
@@ -15,9 +24,7 @@
                 @livewire(\Livewire\Livewire::getAlias(get_class($component)))
             @endif
         @elseif ($component instanceof \SolutionForest\GridLayoutPlugin\Components\Grid\Row)
-            <div class="mt-1">
-                <x-grid-layout-plugin::grid.row :row="$component" />
-            </div>
+            <x-grid-layout-plugin::grid.row :row="$component" />
         @else
             {{ $component }}
         @endif
